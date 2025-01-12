9212ad50ead95d563c90b517bcd9cfed62bef7a8
André Lira <andrelira50@gmail.com>
2025-01-12 17:13:06 -0300

    fazendo o javafx funcionar no vscode e fazendo uma tela teste

--- src/App.java
+++ src/App.java
@@ -1,5 +1,34 @@
-public class App {
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+
+public class App extends javafx.application.Application {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        primaryStage.setTitle("teste");
+
+        // Botão com evento de clique
+        Button button = new Button("teste de botão");
+        button.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent arg0) {
+                System.out.println("Clicou no botão");
+            }
+        });
+
+        // Layout e cena
+        StackPane root = new StackPane();
+        root.getChildren().addAll(button);
+        primaryStage.setScene(new Scene(root, 300, 250));
+
+        // Mostra a janela
+        primaryStage.show();
     }
 }
